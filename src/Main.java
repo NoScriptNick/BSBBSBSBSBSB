1b556ea1548a8122b0b7211aa783ce471483ab7e
613668 <beres.613668@swl.k12.oh.us>
2023-10-06 09:27:50 -0400

    weoithrg

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,24 @@ public class Main {
         System.out.println("Enter your account balance: ");
         if (scan.hasNextDouble()) {
             acct = scan.nextDouble();
+            acctBal = acct;
             scan.nextLine();
             do { //withdrawal or deposit
                 System.out.println("Would you like to withdrawal or deposit? Enter [Y/N]: ");
                 continueYN = scan.nextLine();
-                if (continueYN.equalsIgnoreCase("N")) {
+                if (continueYN.equals("N")) {
                     done = true;
-                } else if (continueYN == "Y") {
+                } else if (continueYN.equals("Y")) {
                     System.out.println("Enter - and a number you would like to withdrawal, or a + and a number you would like to deposit: ");
-                    acctAddMinus = scan.nextDouble();
+                    if (scan.hasNextDouble()) {
+                        acctAddMinus = scan.nextDouble();
+                    }
                 } else {
-                    System.out.println("THATS NOT EITHER YES OR NO!!!!!!!!");
+                    System.out.println("Not even a yes or a no!!!!!");
                 }
             } while (!done);
         } else {
-            System.out.println("THATS NOT A BALANCE");
+            System.out.println("THAT'S NOT A BALANCE!!!!");
         }
     }
 }
