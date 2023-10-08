f4d5fda20292ee83a265bf5d234abd560d12eb95
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-10-08 11:53:54 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,26 +15,45 @@ public class Main {
         System.out.println("Enter your account balance: ");
         if (scan.hasNextDouble()) {
             acct = scan.nextDouble();
-            acctBal = acct;
+            acctBal = 0;
             scan.nextLine();
             do { //withdrawal or deposit
-                System.out.println("Would you like to withdrawal or deposit? Enter [Y/N]: ");
+                System.out.println("Would you like to continue? Enter [Y/N]: ");
                 continueYN = scan.nextLine();
                 if (continueYN.equals("N")) {
                     done = true;
                 } else if (continueYN.equals("Y")) {
                     System.out.println("Would you like to withdrawal or deposit? Enter [W/D]: ");
-                    if (scan.hasNextDouble()) {
-                        acctAddMinus = scan.nextDouble();
+                    continueWD = scan.nextLine();
+                    if (continueWD.equals("W")) {
+                        System.out.println("How much would you like to withdrawal?");
+                        if (scan.hasNextDouble()) {
+                            acctAddMinus = scan.nextDouble();
+                            acctBal = acct - acctAddMinus;
+                            System.out.println("Your new account balance is $" + acctBal);
+                            scan.nextLine();
+                        } else {
+                            System.out.println("Not even a number pal");
+                        }
+                    } else if (continueWD.equals("D")) {
+                        System.out.println("How much would you like to deposit?");
+                        if (scan.hasNextDouble()) {
+                            acctAddMinus = scan.nextDouble();
+                            acctBal = acct + acctAddMinus;
+                            System.out.println("Your new account balance is $" + acctBal);
+                            scan.nextLine();
+                        } else {
+                            System.out.println("Not even a number pal");
+                        }
                     } else {
                         System.out.println("Not even close buddy");
                     }
                 } else {
-                    System.out.println("Not even a yes or a no!!!!!");
+                    System.out.println("Not even a yes or no!!!!!");
                 }
             } while (!done);
         } else {
-            System.out.println("THAT'S NOT A BALANCE!!!!");
+            System.out.println("That's not a balance!!!!!");
         }
     }
 }
